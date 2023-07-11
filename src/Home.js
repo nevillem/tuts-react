46a516b7995f2f87bfec6fb8ec00aa597f90baa4
nevillem <nevillemwije@gmail.com>
2023-07-11 11:47:16 +0300

    added use of props, useEffects

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogIist from "./bloglist";
 
 const Home = () => {
-    const [blog, setBlog]=useState([
+    const [blogs, setBlog]=useState([
         {title:"my new website",body:"lorem ispum",author:"Mario", id:1},
         {title:"Welcome party",body:"lorem ispum",author:"Yoshi", id:2},
         {title:"web development toop tips",body:"lorem ispum",author:"Mario", id:3}
     ])
     const [name, setName]=useState('Mario')
+    const handleDelete=(id)=>{
+        console.log("hello" + id);
+        const newBlogs=  blogs.filter(blog=> blog.id !==  id);
+        setBlog(newBlogs);
+    }
+    useEffect(()=>{
+    console.log(name);
+    },[name]);
+    /*
     const [age, setAge]=useState('25')
     const handleClick=()=>{
         setName('Luigi')
@@ -15,16 +24,21 @@ const Home = () => {
     }
     const handleClickAgain=(name, e)=>{
         console.log("hello," + name , e.target);
-    }
+    }*/
     return ( 
-    <div className="home">
-   <BlogIist blogs={blog} title="All blogs"></BlogIist>
-        <h2>Homepage</h2>
-        <p>{name} is {age} years old</p>
-        <button onClick={handleClick}>Click me</button>
-        <button onClick={(e)=>
-            handleClickAgain("mario", e)
-        }>Click me again</button>
+//     <div className="home">
+//    <BlogIist blogs={blog} title="All blogs"></BlogIist>
+//         <h2>Homepage</h2>
+//         <p>{name} is {age} years old</p>
+//         <button onClick={(e)=>
+//             handleClickAgain("mario", e)
+//         }>Click me again</button>
+//         </div>
+<div className="home">
+        <BlogIist blogs={blogs} title="All blogs" handleDelete={handleDelete}></BlogIist>
+        <button onClick={()=>setName('Luigi')}>Click me</button>
+        <p>{name}</p>
+        {/* <BlogIist blogs={blogs.filter((blog)=>blog.author==='Mario')}/> */}
         </div>
     );
 }
--- src/bloglist.js
+++ src/bloglist.js
@@ -1,16 +1,18 @@
-const BlogIist = (props) => {
-    const blog= props.blogs;
-    const title= props.title;
-    console.log(props.title);
+const BlogIist = ({blogs, title, handleDelete}) => {
+    // const blog= props.blogs;
+    // const title= props.title;
+    // console.log(props);
+
     return ( 
         <div className="blog-list">
-         {
-        blog.map((blog)=>(
-        <div className="blog-preview" key={blog.id}>
-
         <h2>{title}</h2>
+
+         {
+        blogs.map((blog)=>(
+        <div className="blog-preview" key={blog.id}>
             <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
+            <p>Written by { blog.author }</p>
+            <button onClick={()=>handleDelete(blog.id)}>Delete blog</button>
         </div>
         ))
         }           
