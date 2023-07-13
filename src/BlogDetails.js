ae1d16c178eb0cf12ea2f7b106534b3f13d07077
nevillem <nevillemwije@gmail.com>
2023-07-13 23:58:24 +0300

    Reusing Hooks

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,19 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const {data:blog, error, isPending}=useFetch('http://localhost:8000/blogs/'+id);
 
   return (
     <div className="blog-details">
-      <h2>Blog details - { id }</h2>
-    </div>
+        {error && <div> {error}</div>}
+        {isPending && <div> Loading... </div>}
+        {blog && <article> <h2>{blog.title}</h2>
+        <p>{blog.author}</p>
+        <div>{blog.body}</div>
+        </article>}
+     </div>
   );
 }
  
